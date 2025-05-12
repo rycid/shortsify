606c3d508e309fbb6de8d84eb0c4c281c9e9240d
rycid <57729652+rycid@users.noreply.github.com>
2025-05-12 04:47:04 -0400

    Enhance processVideo function to include speed setting and ensure openFileLocation is exposed in electronAPI

--- preload.js
+++ preload.js
@@ -3,8 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   selectVideo: () => ipcRenderer.invoke('select-video'),
   selectOutputPath: (defaultFileName) => ipcRenderer.invoke('select-output-path', defaultFileName),
-  processVideo: (input, output) => ipcRenderer.invoke('process-video', input, output),
+  processVideo: (input, output, speedSetting) => ipcRenderer.invoke('process-video', input, output, speedSetting),
   onProgressUpdate: (callback) => ipcRenderer.on('progress-update', callback),
   checkFileExists: (filePath) => ipcRenderer.invoke('check-file-exists', filePath),
-  showConfirmDialog: (options) => ipcRenderer.invoke('show-confirm-dialog', options)
+  showConfirmDialog: (options) => ipcRenderer.invoke('show-confirm-dialog', options),
+  openFileLocation: (filePath) => ipcRenderer.invoke('open-file-location', filePath)
 });
